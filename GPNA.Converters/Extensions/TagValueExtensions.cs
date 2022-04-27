65486d6a6aadd76473c7fcd5d287f903bfc5c501
Рустам Ишмухаметов <rig12@rambler.ru>
2022-04-27 10:52:23 +0300

    feat - TagValueExtensions is added

---
+++ GPNA.Converters/Extensions/TagValueExtensions.cs
@@ -0,0 +1,16 @@
+﻿using GPNA.Converters.TagValues;
+
+namespace GPNA.Converters.Model
+{
+    public static class TagValueExtensions
+    {
+        public static string GetValue(this TagValue tagvalue)
+        {
+            var type = tagvalue.GetType();
+            var prop = type.GetProperty("Value");
+            var value = prop?.GetValue(tagvalue, null);
+
+            return value?.ToString();
+        }
+    }
+}
