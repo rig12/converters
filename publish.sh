2034c000e915c294e7a779c597592aaeffa0b8fe
Rustam Ishmukhametov <rustam.ishmukhametov@gmail.com>
2022-04-27 06:56:36 +0000

    New file publish.sh

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Configure credentials"
+dotnet nuget add source $FEED_URL -n space -u "%JB_SPACE_CLIENT_ID%" -p "%JB_SPACE_CLIENT_SECRET%" --store-password-in-clear-text
+VERSION=1.0.$JB_SPACE_EXECUTION_NUMBER
+
+echo RUN BUILD...
+dotnet build  
+                
+echo "Publish nuget package"
+cd dotnet
+dotnet pack -p:PackageVersion=$VERSION -o ./
+dotnet nuget push GPNA.Templates.$VERSION.nupkg -s space
