d0e6f062b15a0b06d239b60dbd96c4b123fcd36a
Rustam Ishmukhametov <rustam.ishmukhametov@gmail.com>
2022-09-12 11:23:31 +0000

    gpnacommon

--- .space.kts
+++ .space.kts
@@ -2,10 +2,7 @@ job(".NET Core GPNA.Converters build,test and publish"){
     container(image = "mcr.microsoft.com/dotnet/sdk:5.0"){
         env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json"
         shellScript {
-            content = """
-                echo DOTNET ADD GPNA.COMMON...
-                dotnet add ./GPNA.Converters package GPNA.Common --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json
-                
+            content = """                
                 echo DOTNET ADD GPNA.EXTENSIONS...
                 dotnet add ./GPNA.Converters package GPNA.Extensions --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json
                 
