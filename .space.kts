fc98ff898b6a62d195141b54763904008d4458d4
Rustam Ishmukhametov <rustam.ishmukhametov@gmail.com>
2022-09-12 11:09:00 +0000

    gpnacommon

--- .space.kts
+++ .space.kts
@@ -1,13 +1,13 @@
 job(".NET Core GPNA.Converters build,test and publish"){
     container(image = "mcr.microsoft.com/dotnet/sdk:5.0"){
-        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/common/v3/index.json"
+        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json"
         shellScript {
             content = """
                 echo DOTNET ADD GPNA.COMMON...
-                dotnet add ./GPNA.Converters package GPNA.Common --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/common/v3/index.json
+                dotnet add ./GPNA.Converters package GPNA.Common --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json
                 
                 echo DOTNET ADD GPNA.EXTENSIONS...
-                dotnet add ./GPNA.Converters package GPNA.Extensions --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/common/v3/index.json
+                dotnet add ./GPNA.Converters package GPNA.Extensions --source https://nuget.pkg.jetbrains.space/gpna/p/gpna-common/gpnacommon/v3/index.json
                 
 				echo RUN TESTS...
                 dotnet test ./GPNA.Converters.Tests/
